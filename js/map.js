7b08ef356de3c9261c4e99606468e3eda8b4db97
Douglas James <currentxchange.com@gmail.com>
2020-03-26 14:31:07 -0500

    Forces user to choose a Genre, Mood, and Format

--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
 //--- Locations for First Load ---\\
-
 randLocs = [
   [39.295392, -76.623935],
   [40.644512, -73.978228],
@@ -23,6 +22,7 @@ randLocs = [
   //http://localhost:8888/cxc/?locLat=38.90813299596705&locLng=-96.37207031250001&zoom=5
   startGeo = [38.90813299596705, -96.37207031250001];
 
+  $.ajaxSetup({ cache: true });
 
 
 // --- How would you like that map, sir? --- \\
@@ -61,8 +61,12 @@ $("#close-jumbo").click(function() {
   $("meta[property='og:image'], meta[name='twitter:image:src'], meta[name='twitter:image']").attr('content', 'images/cXc-Alpha-App-1[600].png');
   $("meta[name='description'], meta[property='og:description'], meta[name='twitter:description']").attr('content', 'Find underground music from around the world on the cXc Music Mapp (Alpha)');
   $("meta[property='og:title'], meta[name='twitter:title']").attr('content', 'cXc Music Mapp (Alpha)');
-  $("#selectors-holder").show();
+  if(!$("#slide-left").is(":visible")){
+    $("#selectors-holder").show();
+  }
   $("#bottom-menus").show();
+
+  $("#currency-wrapper").removeClass("open-music-desk");
 
 });
 
@@ -70,12 +74,14 @@ $("#close-add-music").click(function() {
   $("#slide-left").hide();
   $("#selectors-holder").show();
   $("#bottom-menus").show();
+
 });
 
 $("#close-charts").click(function() {
   $("#charts-jumbo").hide();
-  $("#selectors-holder").show();
-  $("#bottom-menus").show();
+  if(!$("#slide-left").is(":visible")){
+    $("#selectors-holder").show();
+  }
 });
 
 
@@ -96,8 +102,6 @@ $("#active-music-return-button").click(function() {
   $("#slide-left").hide();
   $("#active-music-jumbo").show();
 });
-
-
 
 cxcCircleMarker = L.CircleMarker.extend({
   options: {
@@ -136,14 +140,27 @@ mymap.on('dblclick', function(e) {
     $("#add-music-success").hide();
     $("input#location").remove();
     $("#slide-left").show();
+    $("#selectors-holder").hide();
     $("#submit-music").append('<input id="location" valueLat="' + e.latlng.lat + '" valueLng="' + e.latlng.lng + '"  style="display:none"></input>').show();
   });
 });
 
-mymap.on('click', function(e) {
+mymap.on('click', function(event) {
   if (typeof addMusicMarker !== 'undefined') { // Remove Previous Marker & Stored location
     addMusicMarker.remove();
   }
+
+  $("#genre-selector-ul,  #mood-selector-ul, #format-selector-ul, .alert-dismissible, #settings-menus").hide();
+
+  if($("#cookie-warn:visible").length != 0){
+    $("#cookie-warn").fadeOut(222);
+    Cookies.set('dunToldYa', 1,
+      {
+        expires: 9999
+      });
+    event.stopPropagation();
+  }
+
 });
 
 
@@ -185,108 +202,16 @@ makeLocCookieFull = function()
       {
         delete urlGet[key];
       }
-      console.log($.param(urlGet));
+
       history.pushState(null, '', (location.protocol+'//'+location.host+location.pathname + "?" + $.param(urlGet) ));
     });
   }
 
-}
+};
 
 
 // --- COOKIE ALERT!!! ---\\
 if (Cookies.get('dunToldYa') == undefined)
 {
-  $("#alert-holder")
-  .append('<div id="cookie-warn" class="alert alert-dark bottom-alert" role="alert">You like cookies? We do. By clicking anywhere on this site, you agree to our <a href="https://currentxchange.com/cookie-policy/" target="_blank" class="alert-link">cookie policy.</a> Also check out our <a href="https://currentxchange.com/privacy-policy/" target="_blank" class="alert-link">privacy policy</a>. Click this message to dismiss it for good!</div>');
-  $("#cookie-warn").fadeIn(222);
-}
-
-
-$("#cookie-warn").click(function(event){
-    $("#cookie-warn").fadeOut(222);
-
-    Cookies.set('dunToldYa', 1,
-      {
-        expires: 999999
-      });
-    event.stopPropagation();
-  });
-
-$("#cookie-warn a").click(function(event){
-    event.stopPropagation();
-  });
-
-
-// --- Kickstarter Alert ---\\
-
-  date1 = new Date ( 2019, 2, 20 );
-  curDate = new Date();
-
-  if ((Cookies.get('dunToldYa') !== undefined) && Cookies.get('canWeKickIt') == undefined && (curDate <  date1) )
-  {
-    $("#alert-holder")
-    .append('<div id="kick-notice" class="alert alert-success bottom-alert" role="alert">You may notice some <a href="https://currentxchange.com/cxc-music" target="_blank" class="alert-link">features</a> are missing, like playlists, top charts, and Purple. Help fund our vision by supporting our <a href="https://www.kickstarter.com/projects/currentxchange/cxc-music-a-global-map-of-local-music" target="_blank" class="alert-link">kickstarter</a> and see these features very soon! Click this message to dismiss it for good!</div>');
-    $("#kick-notice").fadeIn(222);
-  }
-
-
-  $("#kick-notice").click(function(event){
-      $("#kick-notice").fadeOut(222);
-
-      Cookies.set('canWeKickIt', 1,
-        {
-          expires: 999999
-        });
-      event.stopPropagation();
-    });
-
-  $("#kick-notice a").click(function(event){
-      event.stopPropagation();
-    });
-
-
-
-$("div.selected").ready(function() {
-  $("div.selected").removeClass("highlight");
-});
-
-
-
-
-// === Responsive === \\
-$(window).bind("resize", function () {
-    console.log($(this).width())
-    fontSizeToggler();
-    if ($(this).width() < 481) {
-
-        $('#active-music-jumbo, #slide-left, #charts-jumbo').addClass('container-fluid')
-        $('iframe').attr('width', '100%') //.addClass('row')
-
-        // --- Active Music Return --- \\
-        if($("#title-jumbo").text() == "Loading..."){
-          $("#active-music-return-button").hide();
-        } else {
-          $("#active-music-return-button").show();
-        }
-      //  $('.ytvid iframe').attr('height', '100%')
-    } else if ($(this).width() > 480) {
-      //$('#active-music-jumbo, #slide-left, #charts-jumbo').removeClass('container-fluid')
-    }
-}).trigger('resize');
-
-
-
-
-// --- Fonts in Charts --- \\
-
-function fontSizeToggler()
-{
-  if ($( "#charts-jumbo" ).width() < 300)
-  {
-    console.log("less than fired");
-    $('h2.chart-title').css("font-size","1em")
-  } else {
-        console.log("greater than fired");
-    $('h2.chart-title').css("font-size","2em")
-  }
-}
+  let velcomMessage = '<div id="cookie-warn" class="alert alert-light bottom-alert" role="alert">';
+  velcomMessage += "<h1>Welcome to cXc Music ðŸ—ºï¸
